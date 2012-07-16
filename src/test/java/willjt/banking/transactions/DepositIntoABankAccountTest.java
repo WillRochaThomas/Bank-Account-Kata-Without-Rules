a1ecb76fca223c83f3159e2e8a6cce0e95187873
Will Thomas <withomas214@gmail.com>
2012-07-16 16:37:50 +0100

    Removed unnecessary encapsulation in DepositIntoABankAccountTest
    Hiding test implementation actually made them harder to read.

--- src/test/java/willjt/banking/transactions/DepositIntoABankAccountTest.java
+++ src/test/java/willjt/banking/transactions/DepositIntoABankAccountTest.java
@@ -1,43 +1,21 @@
 package willjt.banking.transactions;
 
-import org.junit.*;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.Test;
 import willjt.banking.entities.BankAccount;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class DepositIntoABankAccountTest {
 
-    private DepositIntoABankAccount deposit;
     private static final int AMOUNT_TO_DEPOSIT = 13;
-    private static final int NEGATIVE_AMOUNT = -7;
-    private static final int ZERO_AMOUNT = 0;
-    @Mock private BankAccount account;
-
-
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     public void shouldAddTheCorrectAmountToTheAccountBalanceWhenADepositIsExecuted() throws Exception {
-        givenADepositIntoABankAccountOfTheAmount(AMOUNT_TO_DEPOSIT);
-        whenTheDepositIsExecuted();
-        thenTheAmountAddedToTheAccountBalanceIs(AMOUNT_TO_DEPOSIT);
-    }
+        BankAccount ourBankAccount = mock(BankAccount.class);
+        DepositIntoABankAccount deposit = new DepositIntoABankAccount(ourBankAccount, AMOUNT_TO_DEPOSIT);
 
-    private void givenADepositIntoABankAccountOfTheAmount(int amountToWithdraw) {
-        deposit = new DepositIntoABankAccount(account, amountToWithdraw);
-    }
-
-    private void whenTheDepositIsExecuted() {
         deposit.execute();
-    }
 
-    private void thenTheAmountAddedToTheAccountBalanceIs(int amountThatShouldBeAdded) {
-        verify(account, times(1)).addToBalance(amountThatShouldBeAdded);
+        verify(ourBankAccount, times(1)).addToBalance(AMOUNT_TO_DEPOSIT);
     }
 }
