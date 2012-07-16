255749a1a51d8c4b41daac8e2e3f5453ca8582c0
Will Thomas <withomas214@gmail.com>
2012-07-16 16:29:46 +0100

    Removed unnecessary encapsulation in MoneyTransferBetweenBankAccountsTest
    Hiding test implementation actually made them harder to read.

--- src/test/java/willjt/banking/transactions/MoneyTransferBetweenBankAccountsTest.java
+++ src/test/java/willjt/banking/transactions/MoneyTransferBetweenBankAccountsTest.java
@@ -3,63 +3,32 @@ package willjt.banking.transactions;
 import org.junit.Test;
 import willjt.banking.entities.BankAccount;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class MoneyTransferBetweenBankAccountsTest {
 
-    private BankAccount mockPayingAccount;
-    private BankAccount mockReceivingAccount;
-    private int theAmount;
-    private static final int AN_AMOUNT = 43;
+    private static final int SOME_TRANSFER_AMOUNT = 43;
 
 
     @Test
     public void shouldDeductTheCorrectAmountFromTheBalanceOfThePayingAccountWhenExecuted() throws Exception {
-        givenAPayingAccount();
-        givenAReceivingAccount();
-        givenAnAmount(AN_AMOUNT);
+        BankAccount payingAccount = mock(BankAccount.class);
+        BankAccount receivingAccount = mock(BankAccount.class);
+        MoneyTransferBetweenBankAccounts theTransfer = new MoneyTransferBetweenBankAccounts(payingAccount, SOME_TRANSFER_AMOUNT, receivingAccount);
 
-        whenTheTransferIsExecuted();
+        theTransfer.execute();
 
-        thenTheFollowingAmountIsDeductedFromTheBalanceOfThePayingAccount(AN_AMOUNT);
+        verify(payingAccount, times(1)).deductFromBalance(SOME_TRANSFER_AMOUNT);
     }
 
     @Test
     public void shouldAddTheCorrectAmountToTheBalanceOfTheReceivingAccountWhenExecuted() throws Exception {
-        givenAPayingAccount();
-        givenAReceivingAccount();
-        givenAnAmount(AN_AMOUNT);
+        BankAccount payingAccount = mock(BankAccount.class);
+        BankAccount receivingAccount = mock(BankAccount.class);
+        MoneyTransferBetweenBankAccounts theTransfer = new MoneyTransferBetweenBankAccounts(payingAccount, SOME_TRANSFER_AMOUNT, receivingAccount);
 
-        whenTheTransferIsExecuted();
-
-        thenTheFollowingAmountIsAddedToTheBalanceOfTheReceivingAccount(AN_AMOUNT);
-    }
-
-
-    private void givenAnAmount(int anAmount) {
-        theAmount = anAmount;
-    }
-
-    private void givenAPayingAccount() {
-        mockPayingAccount = mock(BankAccount.class);
-    }
-
-    private void givenAReceivingAccount() {
-        mockReceivingAccount = mock(BankAccount.class);
-    }
-
-    private void whenTheTransferIsExecuted() {
-        MoneyTransferBetweenBankAccounts theTransfer = new MoneyTransferBetweenBankAccounts(mockPayingAccount, theAmount, mockReceivingAccount);
         theTransfer.execute();
-    }
 
-    private void thenTheFollowingAmountIsDeductedFromTheBalanceOfThePayingAccount(int amountThatShouldHaveBeenDeducted) {
-        verify(mockPayingAccount, times(1)).deductFromBalance(amountThatShouldHaveBeenDeducted);
-    }
-
-    private void thenTheFollowingAmountIsAddedToTheBalanceOfTheReceivingAccount(int amountThatShouldHaveBeenAdded) {
-        verify(mockReceivingAccount, times(1)).addToBalance(amountThatShouldHaveBeenAdded);
+        verify(receivingAccount, times(1)).addToBalance(SOME_TRANSFER_AMOUNT);
     }
 }
